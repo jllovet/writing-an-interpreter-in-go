0318839d17743f4ca2252268645e832d6f5bf9ba
Jonathan Llovet <jllovet@openingquestions.com>
2026-01-01 18:36:25 -0500

    Fix typo in field description

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -6,7 +6,7 @@ import (
 
 type Lexer struct {
 	input        string
-	position     int  // current poision in input (points to current char)
+	position     int  // current position in input (points to current char)
 	readPosition int  //current reading position in input (after current char)
 	ch           byte // current char under examination
 }
